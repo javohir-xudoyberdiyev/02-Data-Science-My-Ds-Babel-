140de7d5a25663e3b53d052d6742dd404768bf18
User <xjavoxir53@gmail.com>
2024-05-10 10:59:51 -0400

    Update my_ds_babel.py

--- my_ds_babel.py
+++ my_ds_babel.py
@@ -1,89 +1,29 @@
-import sqlite3 as sql
+import sqlite3
 import csv
 import pandas as pd
-
-
+from io import StringIO
 
 def sql_to_csv(database, table_name):
-    """Converting from slqite databese top csv file"""
-    con = sql.connect(database)
-    cur = con.cursor()
-    df = pd.read_sql(f"SELECT * FROM {table_name}", con)
-    return df[1:].to_csv(index=False)
+    # Connect to the database
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+    cursor.execute(f"PRAGMA table_info({table_name})")
+    column_names = [row[1] for row in cursor.fetchall()]
+    cursor.execute(f"SELECT * FROM {table_name}")
+    rows = cursor.fetchall()
+    csv_string = ','.join(column_names) + '\n'
+    
+    for row in rows:
+        csv_string += ','.join(map(str, row)) + '\n'
 
+    connection.close()
+    csv_string = csv_string.strip()
+    return csv_string
 
-def csv_to_sql(csv_context, database, table_name):
-    """Converting from csv file to sqlite database"""
-    con = sql.connect(database)
-    df = pd.read_csv(csv_context)
-    return df.to_sql(name=table_name, con=con, index=False)
-
-print(sql_to_csv('all_fault_line.db','fault_lines'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class MyDsBabel:
-#         def sql_to_csv(self, database, table_name):
-#             con = sql.connect(database)
-#             cur = con.cursor()
-#             cur.execute(f"SELECT * FROM {table_name}")
-#             rows = cur.fetchall()
-#
-#             result = ""
-#
-#             for i in cur.description:
-#                 result += f'{i[0]},'
-#             result += '\n'
-#
-#             for row in rows:
-#                 for i in row:
-#                     result += f"{i},"
-#                 result += '\n'
-#
-#             return result
-#
-#         def csv_to_sql(csv_content, database, table_name):
-#             con = sql.connect(database)
-#             cur = con.cursor()
-#
-#             print(csv_content.readlines())
-#
-#
-# abj = MyDsBabel()
-# # print(abj.sql_to_csv('all_fault_line.db','fault_lines'))
-#
-# csv_content = open("list_volcano.csv")
-# abj.csv_to_sql(csv_content=csv_content, database='volcanos.db', table_name='volcanosy')
+def csv_to_sql(csv_content: str, database_name: str, table_name: str) -> None:
+    data = pd.read_csv(csv_content)
+    
+    # Connect to the SQLite database
+    with sqlite3.connect(database_name) as conn:
+        # Insert the data into the specified table in the database
+        data.to_sql(table_name, conn, if_exists='replace', index=False)
